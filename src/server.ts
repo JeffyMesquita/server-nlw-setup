a44f20e11d4f74fdf31870dfb5f23e81398b7c78
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2023-01-17 09:57:34 -0300

    setup backend done

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,22 @@
-import Fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
 
 const app = Fastify();
 const prisma = new PrismaClient();
 
+app.register(cors);
+
 app.get('/habits', async () => {
-  const habits = await prisma.habits.findMany({
-    where: {
-      title : {
-        startsWith: 'Beber'
-      }
-    }
-  })
-  
-  return {habits};
+  const habits = await prisma.habit.findMany();
+
+  return { habits, message: 'success' };
 });
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('listening on port 3333, HTTP Server running');
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('listening on port 3333, HTTP Server running');
+  });
