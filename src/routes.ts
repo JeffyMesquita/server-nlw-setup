5e58ddfdb5b7a9784bd68c931b1c77ff4b8259e7
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2023-01-19 13:28:20 -0300

    Aula 03 Done

--- src/routes.ts
+++ src/routes.ts
@@ -78,4 +78,106 @@ export async function appRoutes(app: FastifyInstance) {
       data: { possibleHabits, completedHabits },
     });
   });
+
+  app.patch('/habits/:id/toggle', async (request, response) => {
+    // route param => parâmetro de identificação
+
+    const toggleHabitsParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitsParams.parse(request.params);
+
+    const today = dayjs().startOf('day').toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    // buscando na tabela daysHabit para ver se o usuário ja havia
+    // marcado como completo neste dia
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+
+    let toggleHabit;
+
+    if (dayHabit) {
+      // remover hábito
+      toggleHabit = await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+    } else {
+      // completar o hábito neste dia
+      toggleHabit = await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
+
+    const habitToResponse = await prisma.habit.findUnique({
+      where: {
+        id: toggleHabit.habit_id,
+      },
+    });
+
+    return response.status(200).send({
+      message: 'Sucesso ao atualizar hábito',
+      result: 'success',
+      data: habitToResponse,
+    });
+  });
+
+  app.get('/summary', async (request, response) => {
+    // Query mais complexa, mais condições, relacionamentos => SQL na mão (RAW)
+    // Prisma ORM: RAW SQL => SQLite
+
+    const summary = await prisma.$queryRaw`
+      SELECT 
+        D.id, 
+        D.date,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HWD
+          JOIN habits H
+            ON H.id = HWD.habit_id
+          WHERE
+            HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D 
+    `;
+
+    return response.status(200).send({
+      message: 'Sucesso ao listar o resumo de hábitos',
+      result: 'success',
+      data: summary,
+    });
+  });
 }
